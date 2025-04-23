66b1857192ef4a9898458d691e95fbe09fb58c64
DHood27-a11y <hooddeja43@gmail.com>
2025-04-23 12:58:11 -0400

    Added cart and javascript functions

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,27 @@
 /* JavaScript coming soon! */
+let form = document.querySelector("#form");
+let container = document.getElementById("form");
+console.log(form);
+form.addEventListener("submit", onSubmit);
+
+function onSubmit() {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+
+  console.log("our data:", dataObject);
+  form.reset();
+
+  let name = dataObject.fullName;
+  let email = dataObject.Email;
+  let password = dataObject.pw;
+
+  let message = `Welcome, ${name}! You are logged in!`;
+  console.log("Message", message);
+
+  let messageParagraph = document.createElement("p");
+
+  messageParagraph.textContent = message;
+  container.appendChild(messageParagraph);
+}
